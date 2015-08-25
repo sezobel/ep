dc18772afef3f574f196dae7352e9a0a33f44564
Prelang Builder <builder@prelang.com>
2015-08-25 22:58:35 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Profile' to allow for create, update

--- app/admin/profile.rb
+++ app/admin/profile.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Profile do
+  permit_params :name, :number, :email, :city, :state, :zip_code, :university, :degree_of_study, :year, :current_employer, :title, :duties
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
