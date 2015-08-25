2ca025a04ac4152abcd6875684db18040dd016ca
Prelang Builder <builder@prelang.com>
2015-08-25 22:57:57 +0000

    Defined validation for attribute 'email' on model 'Contact'

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,4 +2,5 @@ class Contact < ActiveRecord::Base
   belongs_to :profile
   belongs_to :user
   validates_formatting_of :number, using: :us_phone
+  validates_formatting_of :email, using: :email
 end
