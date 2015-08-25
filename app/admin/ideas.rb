3d0d38a36208908aa1d106c4cb1a2da63952e26a
Prelang Builder <builder@prelang.com>
2015-08-25 22:58:09 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Ideas' to allow for create, update

--- app/admin/ideas.rb
+++ app/admin/ideas.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Ideas do
+  permit_params :project_name, :project_description, :business_plan
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
